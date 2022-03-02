93b3f6378f8da870305b2f5ff73c03b7f39812a2
Sebastian Bogan <sebogh@qibli.net>
2022-03-02 19:18:14 +0100

    fix display key and show result

--- web/bp.js
+++ web/bp.js
@@ -1,29 +1,42 @@
 const annotateDuplicates = (arr) => {
 
-    let u = new Set();
-    let d = new Set();
-    for (const i in arr) {
-        if (!u.has(arr[i].place.name)) {
-            u.add(arr[i].place.name)
-        } else {
-            d.add(arr[i].place.name)
-        }
-    }
+
+    // compute a set of duplicate names
+    let u = new Set(); // set to store names we have seen
+    let d = new Set(); // set to store names that appear several times
     for (const i in arr) {
 
-        if (d.has(arr[i].place.name)) {
-            arr[i].desc = arr[i].place.postcode
+        // get the name of the current place
+        const name = arr[i].place.name;
+
+        // copy name to object root (typeahed likes it there)
+        arr[i].name = name
+
+        // if we have seen this name already add it to the duplicate set
+        if (!u.has(name)) {
+            u.add(name)
         } else {
-            arr[i].desc = ''
+            d.add(name)
         }
     }
+
+    // for each item which with a name that appears several times in the result set (an entry in the duplicates set),
+    // add the postcode as discriminator
+    for (const i in arr) {
+        if (d.has(arr[i].name)) {
+            arr[i].disc = arr[i].place.postcode
+        } else {
+            arr[i].disc = ''
+        }
+    }
+
     return arr
 }
 
 // init Bloodhound
 let colors_suggestions = new Bloodhound({
     datumTokenizer: function(datum) {
-        return Bloodhound.tokenizers.whitespace(datum.place.name);
+        return Bloodhound.tokenizers.whitespace(datum.name);
     },
     queryTokenizer: Bloodhound.tokenizers.whitespace,
     remote: {
@@ -59,16 +72,16 @@ $('#my_search').typeahead({
                     default: // 'highway':
                         str += '<i class="bi bi-geo-alt"></i>'
                 }
-                str += data.place.name
-                if (data.desc !== '') {
-                    str += '<span class="sugestion-discriminator">(' + data.desc + ')</span>';
+                str += data.name
+                if (data.disc !== '') {
+                    str += '<span class="sugestion-discriminator">(' + data.disc + ')</span>';
                 }
                 return str + '</div>';
             }
         },
     })
     .on("typeahead:selected", function (e, datum) {
-        console.log(datum)
+        document.getElementById('result').innerHTML = datum.name + " " + datum.place.houseNumber + ", " + datum.place.postcode + " " + datum.place.city
     });
 
 
