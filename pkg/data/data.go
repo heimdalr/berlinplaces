b6778ee787ccd8c21e7a2e3f6dc5608ee225d196
Sebastian Bogan <sebogh@qibli.net>
2022-03-12 19:30:10 +0100

    use normalizer instead of csv annotations

--- pkg/data/data.go
+++ pkg/data/data.go
@@ -1,36 +1,36 @@
 package data
 
 type District struct {
-	Postcode string `csv:"postcode"`
-	District string `csv:"district"`
+	Postcode string
+	District string
 }
 
 type Street struct {
-	ID       int     `csv:"id"`
-	Name     string  `csv:"name"`
-	Cluster  string  `csv:"cluster"`
-	Postcode string  `csv:"postcode"`
-	Lat      float64 `csv:"lat"`
-	Lon      float64 `csv:"lon"`
-	Length   int     `csv:"length"`
+	ID       int
+	Name     string
+	Cluster  string
+	Postcode string
+	Lat      float64
+	Lon      float64
+	Length   int
 }
 
 type Location struct {
-	Type        string  `csv:"type"`
-	Name        string  `csv:"name"`
-	StreetID    int     `csv:"street_id"`
-	HouseNumber string  `csv:"house_number"`
-	Postcode    string  `csv:"postcode"`
-	Lat         float64 `csv:"lat"`
-	Lon         float64 `csv:"lon"`
+	Type        string
+	Name        string
+	StreetID    int
+	HouseNumber string
+	Postcode    string
+	Lat         float64
+	Lon         float64
 }
 
 type HouseNumber struct {
-	StreetID    int     `csv:"street_id"`
-	HouseNumber string  `csv:"house_number"`
-	Postcode    string  `csv:"postcode"`
-	Lat         float64 `csv:"lat"`
-	Lon         float64 `csv:"lon"`
+	StreetID    int
+	HouseNumber string
+	Postcode    string
+	Lat         float64
+	Lon         float64
 }
 
 type Data struct {
